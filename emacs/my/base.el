8902fe5af8f10a65a381499b6e2b6b51c51e2091
Will Foran <willforan@gmail.com>
2019-10-20 19:17:07 -0400

    emacs: no startup message

--- emacs/my/base.el
+++ emacs/my/base.el
@@ -2,7 +2,7 @@
 (tool-bar-mode 0)
 (menu-bar-mode 0)
 (set-default-font "Source Code Pro 14")
-
+(setq inhibit-startup-screen t)
 ;; shift-insert like terminal: x11 primary clipboard
 (global-set-key (kbd "S-<Insert>") 'my/get-primary)
 
