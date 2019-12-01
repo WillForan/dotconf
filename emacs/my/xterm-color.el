119e39871c5bbec5d20478f5226abfe18cc9e4de
Will Foran <willforan@gmail.com>
2019-12-01 14:42:36 +0000

    wifi interface using ip; emacs: xterm only if dispaly

--- emacs/my/xterm-color.el
+++ emacs/my/xterm-color.el
@@ -1,10 +1,10 @@
 ; 20180808 -- full color support 
 ; https://github.com/atomontage/xterm-color
-(use-package xterm-color
+(when (display-graphic-p) 
+ (use-package xterm-color :ensure t
   :config
    ;(setenv "TERM" "xterm-256color")
    (setq comint-output-filter-functions
       (remove 'ansi-color-process-output comint-output-filter-functions))
    (add-hook 'shell-mode-hook
-          (lambda () (add-hook 'comint-preoutput-filter-functions 'xterm-color-filter nil t)))
-)
+          (lambda () (add-hook 'comint-preoutput-filter-functions 'xterm-color-filter nil t)))))
