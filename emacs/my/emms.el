4099a1f1b8fdec7e9ff4324bb9281a2c6d379fb6
WillForan <willforan@gmail.com>
2019-12-02 15:16:34 -0500

    [wip] emacs/emms: mp3 tags

--- emacs/my/emms.el
+++ emacs/my/emms.el
@@ -3,6 +3,12 @@
   (emms-all)
   (emms-default-players)
   (setq emms-source-file-default-directory "/mnt/storage/Music/")
+  (setq emms-playlist-buffer-name "*Music*")
+  ; (setq emms-info-asynchronously t)
+  ; (require 'emms-info-libtag) 
+  ; (setq emms-info-functions '(emms-info-libtag))
+  ; (use-package emms-info-mediainfo :ensure t)
   ; add libre.fm in ~/.authinfo.gpg
+  ; install mp3info mpg321
 )
 
