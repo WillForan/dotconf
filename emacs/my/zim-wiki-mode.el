5e973abb7ae042675498bc56ee9d8e6d3b5f7e08
WillForan <willforan@gmail.com>
2019-12-26 08:45:45 -0500

    emacs/zim: add header function

--- emacs/my/zim-wiki-mode.el
+++ emacs/my/zim-wiki-mode.el
@@ -11,12 +11,20 @@
   (setq zim-wiki-root "~/notes/PersonalWiki")
   (setq zim-wiki-journal-datestr "Calendar/%Y/%02m.txt"))
 
+(defun my/zim-day-header ()
+  "insert header for today. kuldge: should be done by template"
+  (interactive)
+  (let ((header-level "===")
+	(date (format-time-string "%A %B %02d")))
+   (insert (string-join (list header-level date header-level) " "))))
+
+
 (use-package zim-wiki-mode :defer t
   ; :load-path "~/src/utils/zim-wiki-mode" ;; 20191019 - use quelpa
   :bind ("C-c C-n" . zim-wiki-goto-now)
   :init
     (add-hook 'zim-wiki-mode-hook #'flyspell-mode)
+    (add-hook 'zim-wiki-mode-hook #'(lambda () (git-gutter-mode 0)))
   :config
     (evil-leader/set-key-for-mode 'zim-wiki-mode "z" 'zim-wiki-hydra/body)
-    (my/home-wiki)
-)
+    (my/home-wiki))
