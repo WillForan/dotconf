8da5e4a871b96b35c42f9f0c9d1f14226627ee06
Will Foran <willforan@gmail.com>
2021-06-14 14:59:07 -0400

    💄 emacs: default to dark, monokai

--- emacs/my/theme.el
+++ emacs/my/theme.el
@@ -1,3 +1,4 @@
 ;; dark look
 ;(use-package moe-theme :ensure t :config (load-theme 'moe-dark t))
-(use-package  parchment-theme :ensure t :config (load-theme 'parchment t))
+;(use-package  parchment-theme :ensure t :config (load-theme 'parchment t))
+(use-package  monokai-theme :ensure t :config (load-theme 'monokai t))
