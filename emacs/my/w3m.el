4f941b450fc4912e2ee3861662cbfa9b807f0477
Will Foran <willforan@gmail.com>
2021-10-02 22:33:41 -0400

    ✨ emacs/w3m keychord for phone, avoid modifier

--- emacs/my/w3m.el
+++ emacs/my/w3m.el
@@ -2,10 +2,25 @@
   :config
     (setq w3m-confirm-leaving-secure-page nil)
     ; 20210218 - doesn't work. toggle in config
+    ; 20210930 - dont nconc -- adds too many times
     (setq w3m-command-arguments
-           (nconc w3m-command-arguments
-                  '("-o" "display_link_number=0")))
+          '("-o" "display_link_number=0"))
+    (require 'key-chord)
+    ;; toggles, one diriection: #'evil-exit-emacs-state
+    (key-chord-define w3m-mode-map "jk" #'evil-emacs-state)
+    (key-chord-define w3m-mode-map " x" #'counsel-M-x)
+    (key-chord-define w3m-mode-map " b" #'ivy-switch-buffer)
+    (key-chord-define w3m-mode-map " o" "\C-xo")
+    (key-chord-define w3m-mode-map " c" "\C-c")
+
+    ;; (w3m-lnum-mode 1)
+    ;; (setq w3m-command-arguments
+    ;; 	  (nconc w3m-command-arguments
+    ;; 		 '("-o" "display_link_number=0")))
  :bind
    (:map w3m-mode-map
+	 ;; L masks w3m-lnum-mode. but like ace hints better (chars instead of nums)
+	 (("L" . link-hint-open-link))
+	 (("/" . swiper))
 	 (("<mouse-8>" . w3m-view-previous-page))
 	 (("<mouse-9>" . w3m-view-next-page))))
