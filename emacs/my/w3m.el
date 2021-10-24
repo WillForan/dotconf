372046e32b903bc0c7fd5e5cb52d5bc889c226c1
Will Foran <willforan@gmail.com>
2021-10-24 18:06:41 -0400

    ✨ w3m: add readability filter

--- emacs/my/w3m.el
+++ emacs/my/w3m.el
@@ -9,3 +9,33 @@
    (:map w3m-mode-map
 	 (("<mouse-8>" . w3m-view-previous-page))
 	 (("<mouse-9>" . w3m-view-next-page))))
+  ;; from https://tech.toryanderson.com/2021/06/09/how-to-get-readable-mode-in-emacs-w3m/
+  (defun tsa/readability (url)
+    "Get the Readable.JS version of URL"
+    (interactive "P")
+    (message "readabilizing...")
+    (erase-buffer)
+    (insert (shell-command-to-string (concat "curl " url " 2>/dev/null | readability " url))))
+
+  (defun tsa/w3m-toggle-readability (&arg)
+    "Toggle readibility and reload the current page"
+    (interactive "P")
+    (w3m-toggle-filtering nil)
+    (w3m-reload-this-page)
+    (w3m-toggle-filtering nil))
+
+  (add-to-list 'w3m-filter-configuration '(t "Make page readable" ".*" tsa/readability)))
+
+;; 20211003 http://blog.binchen.org/posts/toggle-http-proxy-in-emacs-w3m.html
+(defun w3m-toggle-env-http-proxy ()
+  "set/unset the environment variable http_proxy which w3m uses"
+  (interactive)
+  (let ((proxy "http://192.168.1.130:8118"))
+    (if (string= (getenv "http_proxy") proxy)
+        ;; clear the proxy
+        (progn
+          (setenv "http_proxy" "")
+          (message "env http_proxy is empty now"))
+      ;; set the proxy
+      (setenv "http_proxy" proxy)
+      (message "env http_proxy is %s now" proxy))))
