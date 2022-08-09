e73d79baf6148ee7cd25d8d3624301b3b802741d
WillForan <willforan@gmail.com>
2022-08-09 08:32:15 -0400

    ✨ emacs/python: python-pytest

--- emacs/my/python.el
+++ emacs/my/python.el
@@ -1,2 +1,25 @@
 (setq python-shell-interpreter "ipython"
       python-shell-interpreter-args "--simple-prompt -i")
+
+(defun my/pysetup ()
+  "Flycheck setup w/keybindings."
+  (interactive)
+  (progn
+    (flycheck-mode)
+    (define-key python-mode-map "\M-n" 'flycheck-next-error)
+    (define-key python-mode-map "\M-p" 'flycheck-previous-error)))
+
+;; prefer flycheck over older flymake
+;; flycheck does useful things with py out of the box
+(add-hook 'python-mode-hook #'my/pysetup)
+
+;; might want set a different path to python.
+;; in .dir-locals.el
+;; ((python-mode . ((python-pytest-executable . "pipenv run python -m pytest"))))
+(use-package python-pytest
+  :ensure t
+  :bind
+  ("C-c t" . python-pytest-file-dwim)
+  ("C-c T" . python-pytest-dispatch))
+
+;; end
