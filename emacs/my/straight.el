15303efe8c0ef5d9f05f9d4b045dc01225ad0273
WillForan <willforan@gmail.com>
2022-09-12 09:25:08 -0400

    🚧 emacs:straight

---
+++ emacs/my/straight.el
@@ -0,0 +1,17 @@
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;; (use-package explain-pause-mode
+;;   :straight (explain-pause-mode :type git :host github :repo "lastquestion/explain-pause-mode")
+;;   :config
+;;   (explain-pause-mode))
