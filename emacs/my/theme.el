cfc1de158450f1471b36028099f8a6c5dbbb6bb3
WillForan <willforan@gmail.com>
2022-09-12 09:23:32 -0400

    💄 emacs: font and themeing

--- emacs/my/theme.el
+++ emacs/my/theme.el
@@ -2,8 +2,12 @@
 ;(use-package  parchment-theme :ensure t :config (load-theme 'parchment t))
 
 ;; dark
-(use-package moe-theme :ensure t :config (load-theme 'moe-dark t))
+;; (use-package moe-theme :ensure t :config (load-theme 'moe-dark t))
+(use-package base16-theme :ensure t :config (load-theme 'base16-eva t))
 ; (use-package  monokai-theme :ensure t :config (load-theme 'monokai t))
-; also see 
+; also see moe-theme:moe-dark
 ; doom-monokai-classic, doom-drakula, 
 ; but doom themes do not increase text size for org mode
+
+;;base16-eva
+;;base16-gruvbox-dark-hard 
