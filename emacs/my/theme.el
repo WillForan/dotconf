d541fc31a720f3e29946f47b6325c38199abe162
Will Foran <willforan@gmail.com>
2022-09-13 11:38:06 -0400

    💄 emacs:theme moe after helm-themes; graphite via quelpa

--- emacs/my/theme.el
+++ emacs/my/theme.el
@@ -2,7 +2,19 @@
 ;(use-package  parchment-theme :ensure t :config (load-theme 'parchment t))
 
 ;; dark
-(use-package moe-theme :ensure t :config (load-theme 'moe-dark t))
+(use-package helm-themes :ensure t)
+(use-package graphite-theme :quelpa
+  (graphite-theme :fetcher github :repo "codemicmaves/graphite-theme"))
+(use-package moe-theme :after helm-themes
+  :defer f
+  :ensure t
+  :init
+  (require 'helm-themes)
+  (helm-themes--load-theme "moe-dark"))
+
+;; 20220809 using base16-eva
+
+;; (use-package moe-theme :ensure t :config (load-theme 'moe-dark t))
 ; (use-package  monokai-theme :ensure t :config (load-theme 'monokai t))
 ; also see 
 ; doom-monokai-classic, doom-drakula, 
