a1280ac2e336baabcfbcc44bade06facb1ba054e
WillForan <willforan@gmail.com>
2023-11-05 14:16:15 -0500

    ♻️ wiki-mail.el: notmuch-wiki-tag uses get-wiki-tag, was ido now ivy

--- emacs/my/wiki-mail.el
+++ emacs/my/wiki-mail.el
@@ -11,15 +11,25 @@
 (define-key zim-wiki-mode-map (kbd "C-c +") #'my/wiki-search-mail)
 
 (defun my/notmuch-mode-tag ()
-  (case major-mode
+  "Determine tag function to use bad on mode."
+  (cl-case major-mode
     ('notmuch-search-mode #'notmuch-search-tag)
     ('notmuch-show-mode #'notmuch-show-tag)
     ('notmuch-tree-mode #'notmuch-tree-tag)))
 
+(defun my/get-wiki-tag ()
+  "Completing read of wiki pages using completeion-canidates. Ignore calendar pages."
+  (let (;; (completion-ignore-case t)
+        (wiki-pages (seq-filter (lambda(x) (not (string-match-p "^:Calendar:" x)))
+                                zim-wiki-completion-canidates)))
+    ;; was ido-completing-read. but completion-ignore-case would need to be more global?
+    (ivy-completing-read "wiki tag:" wiki-pages)))
+
 (defun my/notmuch-wiki-tag ()
+  "Prompt for a tag derived from wiki pages and add to mail."
   (interactive)
   (when-let ((mode-fun (my/notmuch-mode-tag))
-             (wikitag (ido-completing-read "wiki tag:" zim-wiki-completion-canidates)))
+             (wikitag (my/get-wiki-tag)))
     (funcall mode-fun (list (concat "+" wikitag)))))
 
 (define-key notmuch-search-mode-map (kbd "C-c +") #'my/notmuch-wiki-tag)
