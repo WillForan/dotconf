6b7afc120b2f5add3866fa7a44d1368fb7570264
WillForan <willforan@gmail.com>
2023-11-05 14:14:14 -0500

    🔧 emacs: my.el load init includes mail

--- emacs/my/my.el
+++ emacs/my/my.el
@@ -119,6 +119,7 @@
        lisp lint ;; these take a long time
        org
        roam annote
+       mail
        ;; last -- if not applied we know something went wrong
        frame-settings theme)))
 
