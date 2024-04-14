e71fcf7d279ed4dde21ea22cfba66dc0efd78c83
Will Foran <willforan@gmail.com>
2024-04-14 10:19:23 -0400

    🔧emacs: eternal-september

--- emacs/my/base.el
+++ emacs/my/base.el
@@ -150,18 +150,3 @@
 
 ;; news usenet
 (setq gnus-select-method '(nntp "news.eternal-september.org"))
-;; (add-to-list 'gnus-secondary-select-methods '(nntp "localhost"))
-;; (add-to-list 'gnus-secondary-select-methods '(nntp "news.gnus.org"))
-;; (add-to-list 'gnus-secondary-select-methods '(nnml ""))
-;; https://www.emacswiki.org/emacs/GnusFormatting
-(setq
-     gnus-summary-line-format "%U%R%z %(%-15,15f %B%s%)\n"
-     gnus-user-date-format-alist '((t . "%Y-%m-%d %H:%M"))
-     gnus-summary-thread-gathering-function 'gnus-gather-threads-by-references
-     gnus-thread-sort-functions '(gnus-thread-sort-by-date)
-     gnus-sum-thread-tree-false-root ""
-     gnus-sum-thread-tree-indent " "
-     gnus-sum-thread-tree-leaf-with-other "├► "
-     gnus-sum-thread-tree-root ""
-     gnus-sum-thread-tree-single-leaf "╰► "
-     gnus-sum-thread-tree-vertical "│")
