792df8a2f296f83df3200e95ae82628a7b457d5a
Will Foran <willforan@gmail.com>
2026-06-05 19:36:06 -0400

    feat(emacs/llm): agent and copilot

--- emacs/my/llm.el
+++ emacs/my/llm.el
@@ -1,5 +1,19 @@
 (use-package gptel :ensure t
+  :vc ( :url "https://github.com/karthink/gptel"
+        :rev :newest)
   :config
   (gptel-make-gh-copilot "Copilot")
   (setq ;; gptel-model 'claude-3.7-sonnet
       gptel-backend (gptel-make-gh-copilot "Copilot")))
+
+(use-package gptel-agent
+  :vc ( :url "https://github.com/karthink/gptel-agent"
+        :rev :newest)
+  :config (gptel-agent-update))
+;; (use-package aider :ensure t)
+
+;; 20260115, run copilot-install-server
+(use-package copilot
+  :vc (:url "https://github.com/copilot-emacs/copilot.el"
+            :rev :newest
+            :branch "main"))
