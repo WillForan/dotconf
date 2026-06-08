daa664bb3b4287f4c77ed830ced96917c121ba52
WillForan <willforan@gmail.com>
2026-06-08 09:09:46 -0400

    wip(llm.el): agent

--- emacs/my/llm.el
+++ emacs/my/llm.el
@@ -1,8 +1,15 @@
 (use-package gptel :ensure t
   :config
+  ;; :vc (:url "https://github.com/karthink/gptel" :rev :newest)
   (gptel-make-gh-copilot "Copilot")
   (setq ;; gptel-model 'claude-3.7-sonnet
-      gptel-backend (gptel-make-gh-copilot "Copilot")))
+   gptel-backend (gptel-make-gh-copilot "Copilot")))
 
 
 (use-package gptel-magit :ensure t :after gptel)
+
+;; 20260605
+(use-package gptel-agent
+  :vc ( :url "https://github.com/karthink/gptel-agent"
+        :rev :newest)
+  :config (gptel-agent-update))
